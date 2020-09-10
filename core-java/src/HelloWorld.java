3c33f02015bd9fd13bc49a410259337c4c349cb2
Jayram <jayram.rout@hotmail.com>
2020-09-09 21:36:22 -0400

    Changes

--- core-java/src/HelloWorld.java
+++ core-java/src/HelloWorld.java
@@ -1,2 +1,11 @@
 public class HelloWorld {
+    // age is a number
+    // name is a string
+    // distance is a float
+    // m f
+    // y n
+    // male female
+    public static void main(String[] args) {
+        System.out.println("Hello world...".charAt(1));
+    }
 }
---
+++ core-java/src/KeyWorld.java
@@ -0,0 +1,3 @@
+public class KeyWorld {
+    String enumx = "Peter";
+}
